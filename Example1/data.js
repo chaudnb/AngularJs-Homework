e12046bb4535176311ad600c711e1927751fa487
Softech <Softech>
2016-07-11 19:31:31 +0700

    Initial commit

---
+++ Example1/data.js
@@ -0,0 +1,416 @@
+var products = [];
+products.push({
+    id: 1,
+    name: 'Product name 1',
+    imageUrl: 'images/1.jpg',
+    price: 100,
+    discount: 80,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'red'
+        },
+        {
+            color: 'yellow'
+        },
+    ],
+    stock: 0,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 2,
+    name: 'Product name 2',
+    imageUrl: 'images/2.jpg',
+    price: 120,
+    discount: 95,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'gray'
+        },
+        {
+            color: 'blue'
+        },
+        {
+            color: 'pink'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 3,
+    name: 'Product name 3',
+    imageUrl: 'images/3.jpg',
+    price: 110,
+    discount: 100,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'white'
+        },
+        {
+            color: 'black'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 4,
+    name: 'Product name 4',
+    imageUrl: 'images/4.jpg',
+    price: 100,
+    discount: 75,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'red'
+        },
+        {
+            color: 'yellow'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 5,
+    name: 'Product name 1',
+    imageUrl: 'images/5.jpg',
+    price: 100,
+    discount: 80,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'red'
+        },
+        {
+            color: 'yellow'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 6,
+    name: 'Product name 2',
+    imageUrl: 'images/6.jpg',
+    price: 100,
+    discount: 80,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'red'
+        },
+        {
+            color: 'yellow'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 7,
+    name: 'Product name 3',
+    imageUrl: 'images/7.jpg',
+    price: 100,
+    discount: 80,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'red'
+        },
+        {
+            color: 'yellow'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
+
+products.push({
+    id: 8,
+    name: 'Product name 4',
+    imageUrl: 'images/8.jpg',
+    price: 100,
+    discount: 80,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizes: [{
+        id:1,
+        size:'S'
+    },{
+        id:2,
+        size:'M'
+    },{
+        id:3,
+        size:'L'
+    },{
+        id:4,
+        size:'XL'
+    }
+    ],
+    colors: [
+        {
+            color: 'green'
+        },
+        {
+            color: 'red'
+        },
+        {
+            color: 'yellow'
+        },
+    ],
+    stock: 10,
+    detail:'Sed ut eros felis. Vestibulum rutrum imperdiet nunc a interdum. In scelerisque libero ut elit porttitor commodo. Suspendisse laoreet magna nec urna fringilla viverra. 100% Cotton',
+    description: '16" waist, 34" inseam, 10.5" front rise, 8.5" knee, 7.5" leg opening, Measurements taken from size 30, Model wears size 31. Model is 6.2 ',
+    shipping: 'Free on orders of $50 or more',
+    moreImages: [
+        {
+            caption: '',
+            imageUrl: 'images/a.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/b.jpg'
+        },
+        {
+            caption: '',
+            imageUrl: 'images/c.jpg'
+        },
+    ]
+});
